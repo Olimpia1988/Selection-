2bf81660e2d9749218513f4089bfaa71b048d41e
Olimpia <olimpiacompean@prusuit.org>
2018-10-09 13:35:38 -0400

    first commit

--- Selection/main.swift
+++ Selection/main.swift
@@ -21,9 +21,37 @@ import Foundation
 //}
 //print(description)
 //
+//for i in 0..<10 {
+//    print(i)
+//}
+//let base = 3
+//let power = 10
+//var answer = 1
+//
+//for _ in 1...power {
+//    answer *= base
+//}
 
+//print("\(base) to the power of \(power) is \(answer)")
 
+//for index in 1...5 {
+//    print("\(index) times 5 is \(index * 5)")
+//}
 
+//let names = ["Anna", "Alex", "Brian", "Jack"]
+//for name in names {
+//    print("Hello, \(name)!")
+//}
+
+//for i in 1...100 where i % 10 == 5 {
+//    print(i)
+//}
+//var number = 10
+//
+//while number > 1 {
+//    number /= 2
+//    print(number)
+}
 var x = 1 // (x,y)
 var y = 2
 var lowX = 1 //print "inside"
@@ -47,33 +75,35 @@ default:
 }
 
 
+//let coordinate = (x: 4, y: 3) // look at photo!
+
+
 //the point is (1,2)
 
-if lowX <= 1 && highY <= 3 {
-    print ("inside")
-} else {
-    print("outside")
-}
-
+//if lowX <= 1 && highY <= 3 {
+//    print ("inside")
+//} else {
+//    print("outside")
+//
 //var locPoint = (x , y)
 //
 //switch locPoint {
-//case 1
+//case 1:
 //print ("Inside")
 //default:
 //    print ("Outside")
 //}
 
-//switch lowX {
-//case 1...3:
-//print ("X inside")
-//    default:
-//print(" ")
-//}
-//switch highY {
-//case 1...3:
-//    print ("Y is inside")
-//default:
-//    print (" ")
-//}
+switch lowX {
+case 1...3:
+print ("X inside")
+    default:
+print(" ")
+}
+switch highY {
+case 1...3:
+    print ("Y is inside")
+default:
+    print (" ")
+}
 
